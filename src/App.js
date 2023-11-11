fd2eb6d107ad1612212a653770e057ea92f6b465
Kinhkha Tran <kinhktran@gmail.com>
2023-11-11 16:48:22 -0600

    adding App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-//import { StyleSheet } from "react-native";
+import React, { useState, useEffect} from 'react';
+import './styles.css'; // Import the CSS file
 
 const DropdownMenu = () => {
   const [selectedOption, setSelectedOption] = useState(null);
@@ -9,12 +9,36 @@ const DropdownMenu = () => {
     setSelectedOption(option);
   };
 
+  useEffect( () => {
+    // make fetch request to /get-exercises
+    const url = "/get-exercises"
+    fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type" : "application/json",
+        "Accept": "application/json"
+      }
+    })
+      .then(res => {
+        if (res.status === 200){
+          return res.json()
+        }else{
+          console.log(res)
+        }
+      }
+      )
+      .then(data => {
+          // update some state variable that will hold out excersizes
+          console.log(data)
+      })
+  }
+  , [])
   return (
-    <div>
-      <h2>Welcome to WorkoutAThon: {selectedOption}</h2>
-      <select onChange={(e) => handleOptionSelect(e.target.value)}>
+    <div className="dropdown-container"> {/* Apply styles from styles.css */}
+      <h2>WorkoutAThon: {selectedOption}</h2>
+      <select className="dropdown" onChange={(e) => handleOptionSelect(e.target.value)}>
         <option value="" disabled selected>
-          Select a Workout 
+          Select an Workout
         </option>
         {options.map((option, index) => (
           <option key={index} value={option}>
@@ -27,3 +51,4 @@ const DropdownMenu = () => {
 };
 
 export default DropdownMenu;
+
