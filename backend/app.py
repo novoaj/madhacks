ad9ea8b6fa102ef8a6eebb7d0ab2fa5be7c73a6e
Jared Novoa <jnovoa@wisc.edu>
2023-11-11 16:23:48 -0600

    get_excersice endpoint in backend

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 import time
 from flask import Flask, jsonify
-
+import requests
+API_NINJAS_KEY = "R81jW6DVPvXpNQViXsr7QQ==KVthtd2jriW2EP2u"
 app = Flask(__name__)
 
 @app.route("/time", methods=["GET"])
@@ -10,3 +11,30 @@ def get_cur_time():
             "time": 123
             }
         )
+"""
+this function is an api endpoint that the frontend can call in order to grab excercises from https://api-ninjas.com/api/exercises
+should return a JSON object with exercises 
+"""
+@app.route("/get-exercises", methods=["GET"])
+def get_exercises():
+    muscle = "biceps" # allow user to specify on frontend?
+    type_of_exercise = "cardio"
+    url = "https://api-ninjas.com/api/exercises?type=cardio"
+    response = requests.get(url, headers={'X-Api-Key': API_NINJAS_KEY})
+    if response.status_code == 200:
+        return jsonify(
+        {
+            "name": response["name"],
+            "type": response["type"],
+            "difficulty": response["difficulty"],
+            "instructions": response["instructions"]
+            }
+        )
+    else: 
+        return jsonify(
+            {
+            "code": response.status_code
+            }
+            )
+        # error message
+
